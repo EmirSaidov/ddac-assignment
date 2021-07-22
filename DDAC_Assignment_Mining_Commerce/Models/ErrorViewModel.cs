fc85544a3638aec58bac2fa7d8c30e5371f559b8
Jaydon Chong <jaydonchong88@gmail.com>
2021-07-22 23:17:57 +0800

    first commit

---
+++ DDAC_Assignment_Mining_Commerce/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DDAC_Assignment_Mining_Commerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
