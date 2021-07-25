0c8ed425dc35b7f0a418ceadf806f4b90a8156e9
FlyingJetski <joshuaxu71@gmail.com>
2021-07-26 01:13:08 +0700

    updated Product CRUD on imageUri, refactored blob-related operations into methods in BlobService.cs, removed SellerID session

--- DDAC_Assignment_Mining_Commerce/Models/ProductModel.cs
+++ DDAC_Assignment_Mining_Commerce/Models/ProductModel.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,7 +15,10 @@ namespace DDAC_Assignment_Mining_Commerce.Models
 
         public int sellerID { get; set; }
         public virtual SellerModel seller{ get; set; }
-        
+
+        [Display(Name = "Product Image")]
+        public string imageUri { get; set; }
+
         [Display(Name ="Product Name")]
         [Required(ErrorMessage ="Product Name is Required")]
         public string productName { get; set; }
