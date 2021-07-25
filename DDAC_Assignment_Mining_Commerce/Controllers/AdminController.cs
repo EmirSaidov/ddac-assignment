6faae371cbad272e0788373e718c080c9406f695
Jaydon Chong <jaydonchong88@gmail.com>
2021-07-26 06:48:41 +0800

    Added Seller account approval, fixed missing is_approved col

---
+++ DDAC_Assignment_Mining_Commerce/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using DDAC_Assignment_Mining_Commerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDAC_Assignment_Mining_Commerce.Controllers
+{
+    public class AdminController : Controller
+    {
+        MiningCommerceContext _context;
+        public AdminController(MiningCommerceContext _context) {
+            this._context = _context;
+        }
+        public async Task<IActionResult> Approve()
+        {
+            return View(await _context.Seller.Include(seller => seller.user).Where(seller=>!seller.is_approved).ToListAsync());
+        }
+
+        public async Task<IActionResult> ApproveSeller(int id) {
+            Console.WriteLine("==========");
+            Console.WriteLine(id);
+            var seller_entity = _context.Seller.FirstOrDefault<SellerModel>(s => s.ID == id);
+            seller_entity.is_approved = true;
+            await _context.SaveChangesAsync();
+            return RedirectToAction(actionName: "Approve");
+        }
+    }
+}
---
+++ DDAC_Assignment_Mining_Commerce/Migrations/20210725222220_add_Seller_approved_col.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DDAC_Assignment_Mining_Commerce.Migrations
+{
+    public partial class add_Seller_approved_col : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_approved",
+                table: "Seller",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_approved",
+                table: "Seller");
+        }
+    }
+}
