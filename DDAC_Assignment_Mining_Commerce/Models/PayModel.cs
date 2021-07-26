700317167c59bae5dfa7ff1f1e8ad7a8a25700e1
Emir-Saidov <emir@reldyn.co>
2021-07-26 14:22:30 +0800

    progress so far

---
+++ DDAC_Assignment_Mining_Commerce/Models/PayModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDAC_Assignment_Mining_Commerce.Models
+{
+    public class PayModel
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Full Name")]
+        [StringLength(70, MinimumLength = 6, ErrorMessage = "The {0} must be between {2}-{1} chars")]
+        [Required]
+        public string fullname { get; set; }
+
+        [Display(Name = "Phone number")]
+        [Required(ErrorMessage = "Phone Number is Required")]
+        [DataType(DataType.PhoneNumber)]
+        public string phone { get; set; }
+
+        [Display(Name = "Card Number")]
+        [Required(ErrorMessage = "Card Number is Required")]
+        [DataType(DataType.CreditCard)]
+        public string card { get; set; }
+
+        [Display(Name = "CVC Number")]
+        [Required(ErrorMessage = "CVC Number is Required")]
+        [StringLength(3, MinimumLength = 3, ErrorMessage = "The {0} must be between {2}-{1} chars")]
+        public string cvc { get; set; }
+
+        [Display(Name = "Expiration Date")]
+        [Required(ErrorMessage = "Expiration Date is Required")]
+        public string ex_date { get; set; }
+    }
+}
---
+++ DDAC_Assignment_Mining_Commerce/Models/ProductModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDAC_Assignment_Mining_Commerce.Models
+{
+    public class ProductModel
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Product Name")]
+        [Required]
+        public string product_name{ get; set; }
+
+        [Display(Name ="Product Description")]
+        [Required]
+        public string product_description{ get; set; }
+
+        [Display(Name = "Product Image")]
+        [Required]
+        public string product_image { get; set; }
+
+        [Display(Name = "Product Price")]
+        [Required]
+        public string product_price { get; set; }
+
+    }
+}
