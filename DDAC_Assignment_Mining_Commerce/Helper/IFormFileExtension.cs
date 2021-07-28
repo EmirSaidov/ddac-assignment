300633f40d19fe1c2eabc3c2aecb70184103f365
Jaydon Chong <jaydonchong88@gmail.com>
2021-07-28 17:16:36 +0800

    Fixed login analytics

---
+++ DDAC_Assignment_Mining_Commerce/Helper/IFormFileExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDAC_Assignment_Mining_Commerce.Helper
+{
+    public static class IFormFileExtension
+    {
+        public static string getExtension(IFormFile file) {
+            return Path.GetExtension(file.FileName);
+        }
+    }
+}
