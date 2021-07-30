abd0eead2518653643a00450618e4224fb5ff6df
Jaydon Chong <jaydonchong88@gmail.com>
2021-07-30 18:34:13 +0800

    Re-added Bootstrap folder and QueueService

---
+++ DDAC_Assignment_Mining_Commerce/Services/QueueService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDAC_Assignment_Mining_Commerce.Services
+{
+    public class QueueService
+    {
+        private readonly StorageAccountService _storage;
+        public QueueService(StorageAccountService _storage) {
+            this._storage = _storage;
+        }
+
+        public CloudQueueClient getClientAgent()
+        {
+            CloudStorageAccount account = this._storage.connectStorageAccount();
+            return account.CreateCloudQueueClient();
+        }
+
+        public CloudQueue getQueue(string queue_name)
+        {
+            CloudQueue queue = getClientAgent().GetQueueReference(queue_name);
+            return queue.CreateIfNotExistsAsync().Result ? getQueue(queue_name) : queue;
+        }
+    }
+}
