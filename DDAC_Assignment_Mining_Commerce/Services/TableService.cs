c69236024b399099af1a1b396bae89909c399c53
FlyingJetski <joshuaxu71@gmail.com>
2021-07-30 22:35:48 +0700

    added Subscription model, added insert and read methods in TableService

--- DDAC_Assignment_Mining_Commerce/Services/TableService.cs
+++ DDAC_Assignment_Mining_Commerce/Services/TableService.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.WindowsAzure.Storage;
+﻿using Azure;
+using DDAC_Assignment_Mining_Commerce.Models;
+using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DDAC_Assignment_Mining_Commerce.Services
@@ -25,6 +30,50 @@ namespace DDAC_Assignment_Mining_Commerce.Services
         {
             CloudTable table = getClientAgent().GetTableReference(table_name);
             return table.CreateIfNotExistsAsync().Result ? getTable(table_name) : table;
+        }
+
+        public async void Subscribe(Subscription subscription)
+        {
+            Subscription exist = await GetSubscription(subscription.RowKey, subscription.PartitionKey);
+            if (exist == null)
+            {
+                TableOperation tableOperation = TableOperation.Insert(subscription);
+                _ = getTable("subscriptions").ExecuteAsync(tableOperation);
+            } else
+            {
+                subscription.ETag = "*";
+                TableOperation tableOperation = TableOperation.Delete(subscription);
+                _ = getTable("subscriptions").ExecuteAsync(tableOperation);
+            }
+        }
+
+        public async Task<Subscription> GetSubscription(string rowKey, string partitionKey)
+        {
+            TableOperation tableOperation = TableOperation.Retrieve<Subscription>(partitionKey, rowKey);
+            TableResult tableResult = await getTable("subscriptions").ExecuteAsync(tableOperation);
+            return tableResult.Result as Subscription;
+        }
+
+        public async Task<List<Subscription>> GetSubscriptionsByPK(string partitionKey)
+        {
+            List<Subscription> subscriptions = new List<Subscription>();
+            TableQuery<Subscription> partitionScanQuery = new TableQuery<Subscription>().Where
+               (TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, partitionKey));
+
+            TableContinuationToken token = null;
+            // Page through the results
+            do
+            {
+                TableQuerySegment<Subscription> segment = await getTable("subscriptions").ExecuteQuerySegmentedAsync(partitionScanQuery, token);
+                token = segment.ContinuationToken;
+                foreach (Subscription subscription in segment)
+                {
+                    subscriptions.Add(subscription);
+                }
+            }
+            while (token != null);
+
+            return subscriptions;
         }
     }
 }
