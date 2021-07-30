dcf5fc5338bafd9be19acfc667ff3e4204547c55
FlyingJetski <joshuaxu71@gmail.com>
2021-07-31 02:40:26 +0700

    Notification model and worker service

---
+++ DDAC_Assignment_Mining_Commerce/Models/Notification.cs
@@ -0,0 +1,24 @@
+﻿//using Microsoft.WindowsAzure.Storage.Table;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+//namespace DDAC_Assignment_Mining_Commerce.Models
+//{
+//    public class Notification: TableEntity
+//    {
+//        public int sellerID { get; set; }
+//        public int buyerID { get; set; }
+//        public int buyerID { get; set; }
+
+//        public void AssignRowKey()
+//        {
+//            this.RowKey = sellerID.ToString();
+//        }
+//        public void AssignPartitionKey()
+//        {
+//            this.PartitionKey = buyerID.ToString();
+//        }
+//    }
+//}
---
+++ DDAC_Assignment_Mining_Commerce/Services/WorkerService.cs
@@ -0,0 +1,45 @@
+﻿using DDAC_Assignment_Mining_Commerce.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DDAC_Assignment_Mining_Commerce.Services
+{
+    public class WorkerService : BackgroundService
+    {
+        private readonly ILogger<WorkerService> _logger;
+        private readonly IServiceProvider _services;
+
+        public WorkerService(ILogger<WorkerService> logger, IServiceProvider services)
+        {
+            _logger = logger;
+            _services = services;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                using (var scope = _services.CreateScope())
+                {
+                    var ctx = scope.ServiceProvider.GetRequiredService<TableService>();
+
+                    var subscriptions = await ctx.GetSubscriptionsByPK("1");
+                    var count = 1;
+                    foreach(Subscription subscription in subscriptions)
+                    {
+                        Debug.WriteLine(subscription.buyerID);
+                        Debug.WriteLine(count);
+                        count++;
+                    }   
+                }
+            }
+        }
+    }
+}
